27a062e23dd89c0dc2b7d0311928d3323edfc194
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-21 17:33:28 -0400

    Created preliminary game state

--- js/states/game.js
+++ js/states/game.js
@@ -2,3 +2,12 @@ let jab = function (game) {
 
 };
 
+jab.prototype =  {
+  create: function () {
+    let gameOverButton = this.game.add.button(160, 100, "over", this.gameOver, this);
+  },
+
+  gameOver: function () {
+    this.game.state.start("GameOver", true, false);
+  }
+}
