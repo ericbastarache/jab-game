e0f74380ef2b412729576b907a791b81b1b3a71d
Eric Bastarache <eric_bastarache@hotmail.com>
2017-04-21 17:38:08 -0400

    Added pause function to game

--- js/states/game.js
+++ js/states/game.js
@@ -5,9 +5,14 @@ let jab = function (game) {
 jab.prototype =  {
   create: function () {
     let gameOverButton = this.game.add.button(160, 100, "over", this.gameOver, this);
+    let pauseButton = this.game.add.button(160, 200, "pause", this.pause, this);
   },
 
   gameOver: function () {
     this.game.state.start("GameOver", true, false);
+  },
+
+  pause: function () {
+    this.game.state.start("Pause");
   }
 }
